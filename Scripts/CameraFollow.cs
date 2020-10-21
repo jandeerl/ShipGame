05573576df471497f73aef1e09a10a32d0745185
Jan <johnderlukiewicz@gmail.com>
2020-10-21 10:20:45 +0200

    initial commit

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform objectToFollow;
+    [SerializeField]
+    float xOffset = 10f;
+    [SerializeField]
+    float zOffset = 10f;
+    [SerializeField]
+    float height = 10f;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (objectToFollow != null)
+        {
+            transform.LookAt(objectToFollow);
+            transform.position = new Vector3(objectToFollow.position.x + xOffset,
+                objectToFollow.position.y + height, objectToFollow.position.z + zOffset);
+        }
+    }
+}
---
+++ Scripts/MoveShip.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShip : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectToMove;
+
+    // TODO array will be all the children of the parent transform passed here
+    [SerializeField]
+    Transform[] ports;
+
+    Transform nextPort;
+    Vector3 nextPortPos;
+
+    void Start()
+    {
+        nextPortPos = transform.position;
+    }
+
+    void Update()
+    {
+        GoToNextPoint();
+    }
+
+    void GoToNextPoint()
+    {
+        objectToMove.transform.position = Vector3.Lerp(transform.position, nextPortPos, Time.deltaTime);
+    }
+
+    // called by unity event
+    public void StartMoving()
+    {
+        nextPortPos = nextPort.position;
+    }
+
+
+    public void ChooseNextPort(int portNumber)
+    {
+        if (nextPort != ports[portNumber])
+            nextPort = ports[portNumber];
+        else
+        {
+            Array.Reverse(ports);
+            nextPort = ports[portNumber];
+        }
+    }
+}
